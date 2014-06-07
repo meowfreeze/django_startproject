d11071734293af586f01183af2376002be002f57
meowfreeze <meowfreeze@gmail.com>
2014-06-07 15:20:36 -0700

    added static and media serve to urls.py

--- project_name/project_name/urls.py
+++ project_name/project_name/urls.py
@@ -1,12 +1,14 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', '{{ project_name }}.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
 )
+
+# This helper function works only in debug mode
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
