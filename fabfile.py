32c05483939b6a160f7ee641819b291e803172f2
meowfreeze <meowfreeze@gmail.com>
2014-09-30 13:35:46 -0600

    updated fabfile

--- fabfile.py
+++ fabfile.py
@@ -2,44 +2,87 @@ import os
 
 from fabric.api import *
 
-env.hosts = ['']
+env.hosts = ['laislama@laislamarts.org']
 
 # CONFIG
 
-STAGING_DIR = ''
-STAGING_WWW = ''
+# project server structure
+#
+# srv
+# └── {{ project_name }}
+#     ├── production
+#     │   ├── {{ project_name }}
+#     │   ├── fabfile.py
+#     │   ├── README.md
+#     │   └── requirements
+#     └── staging
+#         ├── {{ project_name }}
+#         ├── fabfile.py
+#         ├── README.md
+#         └── requirements
 
-PRODUCTION_DIR = ''
-PRODUCTION_WWW = ''
+# location of manage.py file
+STAGING_DJANGO_ROOT = '~/srv/{{ project_name }}/staging/{{ project_name }}'
+STAGING_ENV = 'staging'
+STAGING_FCGI = 'staging.fcgi'
+STAGING_ROOT = '~/srv/{{ project_name }}/staging'
+STAGING_SETTINGS = '{{ project_name }}.settings.staging'
+STAGING_SRV = '~/public_html/staging'
+
+# location of manage.py file
+PRODUCTION_ENV = 'production'
+PRODUCTION_DJANGO_ROOT = '~/srv/{{ project_name }}/production/{{ project_name }}'
+PRODUCTION_FCGI = 'production.fcgi'
+PRODUCTION_ROOT = '~/srv/{{ project_name }}/production'
+PRODUCTION_SETTINGS = '{{ project_name }}.settings.production'
+PRODUCTION_SRV = '~/public_html/production'
+
 
 # DEPLOYMENT
         
 def deploy(repo):
+    
     # tests?
     
     if repo == 'production':
-        # git
+        
+        # push master to repo
         local('git push origin master')
-        with cd(PRODUCTION_DIR):
+        
+        # pull master to production repo
+        with cd(PRODUCTION_ROOT):
             run('git pull origin master')
             
         # collectstatic
-        with cd(os.path.join(PRODUCTION_DIR, 'still_room')):
-            run('workon [env name] && python manage.py collectstatic --settings=still_room.settings.production')
+        with cd(PRODUCTION_DJANGO_ROOT), \
+            prefix('workon %s' % PRODUCTION_ENV):
+            
+            run('python manage.py collectstatic --settings=%s' \
+                % PRODUCTION_SETTINGS)
         
-        # restart server
+        # restart application
+        with cd(PRODUCTION_SRV):
+            run('touch %s' % PRODUCTION_FCGI)
     
     elif repo == 'staging':
-        # git
+        
+        # push staging branch to repo
         local('git push origin staging')
-        with cd(STAGING_DIR):
+        
+        # pull staging branch to staging repo
+        with cd(STAGING_ROOT):
             run('git pull origin staging')
         
         # collectstatic
-        with cd(os.path.join(STAGING_DIR, 'still_room')):
-            run('workon [env name] && python manage.py collectstatic --settings=still_room.settings.staging')
+        with cd(STAGING_DJANGO_ROOT), \
+            prefix('workon %s' % STAGING_ENV):
+            
+            run('python manage.py collectstatic --settings=%s' \
+                % STAGING_SETTINGS)
         
-        # restart server
+        # restart application
+        with cd(STAGING_SRV):
+            run('touch %s' % STAGING_FCGI)
     
     else:
         print 'enter a valid repo'
