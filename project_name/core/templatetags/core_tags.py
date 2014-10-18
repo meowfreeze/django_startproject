1b12a754402225ef3fe5b1182e233a84d77f588f
meowfreeze <meowfreeze@gmail.com>
2014-10-18 13:35:57 -0600

    added append_to_get template tag to core

--- project_name/core/templatetags/core_tags.py
+++ project_name/core/templatetags/core_tags.py
@@ -1,7 +1,9 @@
 from django import template
 from django.core.urlresolvers import reverse
 
+
 register = template.Library()
+
 
 @register.simple_tag
 def active(request, urls):
@@ -13,3 +15,31 @@ def active(request, urls):
     if request.path in (reverse(url) for url in urls.split()):
         return 'active'
     return ''
+
+
+@register.simple_tag(takes_context=True)
+def append_to_get(context, k, v):
+    """
+    add a get parameter to the parameters already present in the url
+    
+    USAGE: {% append_to_get k v %}
+    
+    """
+    params = context['request'].GET.copy()
+    
+    # prevent duplicate keys
+    params.pop(k, None)
+    
+    template = '%s=%s'
+    append = template % (k, v)
+    
+    if params:
+        prefix = '&'
+    else:
+        prefix = ''
+    
+    result = []
+    for k, v in params.iteritems():
+        result.append(template % (k, v))
+    
+    return '?' + '&'.join(result) + prefix + append
